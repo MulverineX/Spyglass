288ab29f1964cf9c1b814607ec12c9716c43d34e
SPGoding <15277496+SPGoding@users.noreply.github.com>
2023-06-25 12:42:07 -0500

    ♻️ Reactor handling of game versions
    As Mojang now may introduce major technical changes in snapshots, it no
    longer makes sense for us to use a concept of "release version" to keep
    track of the current enabled feature set. To simplify everything, we
    will instead treat all types of versions equally and use a simple
    `GameVersionComparator` .

--- packages/java-edition/src/dependency/common.ts
+++ packages/java-edition/src/dependency/common.ts
@@ -1,34 +1,79 @@
-export type ReleaseVersion = `1.${number}`
-export namespace ReleaseVersion {
+import { atArray } from '@spyglassmc/core'
+
+export interface GameVersion {
 	/**
-	 * @returns
-	 * * `-1` if `a` is older than `b`.
-	 * * `0` if `a` is the same as `b`.
-	 * * `1` if `a` is newer than `b`.
+	 * ID of the version as specified in [`version_manifest.json`](https://piston-meta.mojang.com/mc/game/version_manifest.json).
+	 *
+	 * @example `1.20-rc1`
 	 */
-	export function cmp(a: ReleaseVersion, b: ReleaseVersion): number {
-		return Math.sign(Number(a.slice(2)) - Number(b.slice(2)))
-	}
-}
-
-export interface VersionInfo {
-	release: ReleaseVersion
 	id: string
+	/**
+	 * Human-readable name of the version as specified in the game client jar's `version.json` file.
+	 *
+	 * @example `1.20 Release Candidate 1`
+	 */
 	name: string
-	isLatest: boolean
 }
 
-// DOCS: Update here when format_version is changed.
-/**
- * A map from `pack_format` numbers to a RegExp. The latest version whose `release_target` matches the RegExp should be used.
- */
-export const PackVersionMap: Record<number, RegExp | undefined> = {
-	5: /^1\.15.*$/,
-	6: /^1\.16.*$/,
-	7: /^1\.17.*$/,
-	8: /^1\.18(\.1)?$/,
-	9: /^1\.18.*$/,
-	10: /^1\.19.*$/,
+export class GameVersionComparator {
+	/**
+	 * A map from version IDs to their freshness.
+	 * 
+	 * Freshness: how many versions are released before a vestion.
+	 * Larger freshness -> newer version.
+	 */
+	#freshnessMap: Map<string, number>
+	/**
+	 * The ID of the latest version.
+	 */
+	#latestVersion: string
+
+	/**
+	 * @param versions Sorted from latest to oldest
+	 */
+	constructor(versions: readonly GameVersion[]) {
+		if (!versions.length) {
+			throw new Error('Version list is empty')
+		}
+
+		this.#latestVersion = versions[0].id
+		this.#freshnessMap = new Map()
+		for (const [i, version] of versions.entries()) {
+			this.#freshnessMap.set(version.id, versions.length - i - 1)
+		}
+	}
+
+	/**
+	 * @param a `GameVersion` or version ID.
+	 * @param b `GameVersion` or version ID.
+	 * 
+	 * @returns
+	 * * Negative if `a` is older than `b`
+	 * * 0 if `a` is the same version as `b`
+	 * * Positive if `a` is newer than `b`
+	 */
+	cmp(a: GameVersion | string, b: GameVersion | string): number {
+		const [freshnessA, freshnessB] = [a, b].map((v) => {
+			const freshness = this.#freshnessMap.get(this.#getId(v))
+			if (freshness === undefined) {
+				throw new Error(`Unknown version ID '${this.#getId(v)}'`)
+			}
+			return freshness
+		})
+		return freshnessA - freshnessB
+	}
+
+	/**
+	 * @param v `GameVersion` or version ID.
+	 * @returns If the version is the latest version.
+	 */
+	isLatest(v: GameVersion | string): boolean {
+		return this.#getId(v) === this.#latestVersion
+	}
+
+	#getId(v: GameVersion | string): string {
+		return typeof v === 'string' ? v : v.id
+	}
 }
 
 export interface PackMcmeta {
@@ -38,12 +83,10 @@ export interface PackMcmeta {
 }
 export namespace PackMcmeta {
 	export function assert(data: any): asserts data is PackMcmeta {
-		const format: string | undefined = data?.pack?.pack_format?.toString()
-		if (!format) {
-			throw new Error('“pack.pack_format” undefined')
-		}
-		if (!Object.keys(PackVersionMap).includes(format)) {
-			throw new Error(`Unknown pack_format “${format}”`)
+		const format: unknown = data?.pack?.pack_format
+		// eslint-disable-next-line no-restricted-syntax
+		if (!(typeof format === 'number' && Number.isInteger(format))) {
+			throw new Error('Expected pack.pack_format to be an integer ')
 		}
 	}
 }
